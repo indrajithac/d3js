749a02ebaffe2be3782533093d164ebe4f9490d7
indrajithac <indrajith@juneits.com>
2023-01-21 21:02:00 +0530

    line chart added(didnt get logoc completly)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import LineChart from "./Components/LineChart/LineChart";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <LineChart />
+    </div>
+  );
 }
 
 export default App;
