55ab48234c17a6e5693fb83d282f4bfe7bac4340
indrajithac <indrajith@juneits.com>
2023-01-22 15:49:39 +0530

    curved line graph

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import "./App.css";
+import CurvedLineChart from "./Components/CurvedLineChart/CurvedLineChart";
 import LineChart from "./Components/LineChart/LineChart";
 
 function App() {
+  const [data, setData] = useState([10, 20, 35, 55, 60,10]);
   return (
     <div className="App">
       <LineChart />
+      <CurvedLineChart data={data} />
     </div>
   );
 }
